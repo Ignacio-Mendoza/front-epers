e6c25d24c08f62894bea40f006dc9aa9b0005488
ignacio <nachommendoza@hotmail.com>
2024-11-18 20:26:39 -0300

    inicializando app mobile

---
+++ ouijPers/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>¡Bienvenido a mi aplicación React Native con TypeScript!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
